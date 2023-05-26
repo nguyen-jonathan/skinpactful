496f8e3675f92a790f4f54f0b8de44e3a6e6d9f6
naniwin <njonathan990@gmail.com>
2023-05-26 00:23:05 -0500

    adding AppointmentForm

---
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+
+const AppointmentForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here (e.g., send data to server)
+    // You can access the form values with 'name', 'email', 'date', and 'time' variables
+    console.log('Form submitted:', name, email, date, time);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="time">Time:</label>
+        <input
+          type="time"
+          id="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default AppointmentForm;
