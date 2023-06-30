11f2edbd9aed7f543ee942ca00ca5478921dc10d
naniwin <njonathan990@gmail.com>
2023-06-29 21:39:08 -0500

    img for contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import lotionStock from './lotionStock.jpeg';
+import {Link} from 'react-router-dom';
 import SocialMediaLinks from '../SocialMediaLinks/SocialMediaLinks';
 import './Contact.css';
 
 const Contact = () => {
   return (
     <div>
-      <div className="contactPage-section-text-container">
-        <h1 className="primary-heading-contact">Contact us</h1>
+      <div className="contact-section-text-container">
+        <img src={lotionStock} alt="" />
         <br />
-        <p>skinpactfulcoaching@gmail.com</p>
+        <Link
+          to="https://calendly.com/skinpactful/consultation"
+          target="_blank"
+          rel="noopener noreferrer">
+          <button className="secondary-button">
+            Schedule a Free Consultation
+          </button>
+        </Link>
         <br />
         <h2>Find us at our social media outlets</h2>
       </div>
